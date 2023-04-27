8c7b34eac176473003b1a559218db744cc675427
Matthew Moffitt <mmoffitt121@gmail.com>
2023-04-27 10:31:09 -0500

    Script trying

--- SardMantleUI/src/app/map/map.component.ts
+++ SardMantleUI/src/app/map/map.component.ts
@@ -12,6 +12,30 @@ interface LocationType {
   id: number;
   name: string;
 }
+interface Area {
+  id: number;
+  name: string;
+}
+interface Subregion {
+  id: number;
+  name: string;
+}
+interface Region {
+  id: number;
+  name: string;
+}
+interface Subcontinent {
+  id: number;
+  name: string;
+}
+interface Continent {
+  id: number;
+  name: string;
+}
+interface CelestialObject {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-map',
@@ -26,6 +50,12 @@ export class MapComponent implements OnInit {
   private primaryMarkerLayer: any = L.layerGroup();
   private primaryMarkerLayerZoom: number = 6;
   private addMarkerLayer: any = L.layerGroup();
+  private areaLayer: any = L.layerGroup();
+  private subregionLayer: any = L.layerGroup();
+  private regionLayer: any = L.layerGroup();
+  private subcontinentLayer: any = L.layerGroup();
+  private continentLayer: any = L.layerGroup();
+  //private markersCanvas: any = L.MarkersCanvas();
   public placing = false;
   public addLocationMarkerIcon = 'add';
   public locationTypes: LocationType[] = [];
@@ -39,6 +69,20 @@ export class MapComponent implements OnInit {
   addNameControl = new FormControl('', [Validators.required, Validators.maxLength(1000)]);
   addLocationTypeControl = new FormControl('', []);
 
+  public areas: Area[];
+  public subregions: Subregion[];
+  public regions: Region[];
+  public subcontinents: Subcontinent[];
+  public continents: Continent[];
+  public celestialObjects: CelestialObject[];
+
+  addAreaControl = new FormControl('', []);
+  addSubregionControl = new FormControl('', []);
+  addRegionControl = new FormControl('', []);
+  addSubcontinentControl = new FormControl('', []);
+  addContinentControl = new FormControl('', []);
+  addCelestialObjectControl = new FormControl('', []);
+
   private addLocationDraggableIcon = L.icon({
     iconUrl: 'marker-icon.png',
     iconSize: [25, 41],
@@ -50,6 +94,12 @@ export class MapComponent implements OnInit {
   });
 
   @ViewChild('locationTypeSelect') locationTypeSelect: MatSelect;
+  @ViewChild('areaSelect') areaSelect: MatSelect;
+  @ViewChild('subregionSelect') subregionSelect: MatSelect;
+  @ViewChild('regionSelect') regionSelect: MatSelect;
+  @ViewChild('subcontinentSelect') subcontinentSelect: MatSelect;
+  @ViewChild('continentSelect') continentSelect: MatSelect;
+  @ViewChild('celestialObjectSelect') celestialObjectSelect: MatSelect;
   private selectedLocationType: LocationType;
 
   // Initializes the map
@@ -85,6 +135,7 @@ export class MapComponent implements OnInit {
 
   // Called when the Add icon is clicked. Creates a movable icon for location creation.
   public addPlaceIcon(): void {
+    this.addMapDataTypeControl.setValue(null);
     if (this.placing) {
       this.addLocationMarkerIcon = 'add';
       this.addMarkerLayer.clearLayers();
@@ -137,6 +188,60 @@ export class MapComponent implements OnInit {
     })
   }
 
+  public queryAreas() {
+    this.mapService.getAreas([]).subscribe(data => {
+      this.areas = data;
+    },
+    error => {
+      console.error(error);
+    })
+  }
+
+  public querySubregions() {
+    this.mapService.getSubregions([]).subscribe(data => {
+      this.subregions = data;
+    },
+    error => {
+      console.error(error);
+    })
+  }
+
+  public queryRegions() {
+    this.mapService.getRegions([]).subscribe(data => {
+      this.regions = data;
+    },
+    error => {
+      console.error(error);
+    })
+  }
+
+  public querySubcontinents() {
+    this.mapService.getSubcontinents([]).subscribe(data => {
+      this.subcontinents = data;
+    },
+    error => {
+      console.error(error);
+    })
+  }
+
+  public queryContinents() {
+    this.mapService.getContinents([]).subscribe(data => {
+      this.continents = data;
+    },
+    error => {
+      console.error(error);
+    })
+  }
+
+  public queryCelestialObjects() {
+    this.mapService.getCelestialObjects([]).subscribe(data => {
+      this.celestialObjects = data;
+    },
+    error => {
+      console.error(error);
+    })
+  }
+
   public validateAdd() {
     if (this.addNameControl.invalid) {
       this.addNameControl.markAllAsTouched(); // mark all input fields as touched to trigger error messages
@@ -147,25 +252,124 @@ export class MapComponent implements OnInit {
 
   public createLocation() {
     if (!this.validateAdd()) return;
-    var model = {
-      locationName: this.addNameControl.value,
-      areaId: null,
-      locationTypeId: this.locationTypeSelect.value ? this.locationTypeSelect.value.id : null,
-      latitude: this.addMarkerLat,
-      longitude: this.addMarkerLng
-    };
-    this.mapService.postLocation(model).subscribe(data => {
-      this.clearAllMarkerLayers();
-      this.queryLocations();
-      this.placing = false;
-      this.addLocationMarkerIcon = 'add';
-      this.addMarkerLayer.clearLayers();
-      this.addNameControl.setValue('');
-      this.addNameControl.markAsUntouched();
-    },
-    error => {
-      console.error(error);
-    })
+
+    switch (this.addMapDataTypeControl.value) {
+      // Location
+      case "0":
+        var locationModel = {
+          locationName: this.addNameControl.value,
+          areaId: this.areaSelect.value ? this.areaSelect.value.id : null,
+          locationTypeId: (this.locationTypeSelect && this.locationTypeSelect.value) ? this.locationTypeSelect.value.id : null,
+          latitude: this.addMarkerLat,
+          longitude: this.addMarkerLng
+        };
+        this.mapService.postLocation(locationModel).subscribe(data => {
+          this.handlePosted();
+        },
+        error => {
+          console.error(error);
+        })
+        break;
+
+      // Area
+      case "1":
+        var areaModel = {
+          name: this.addNameControl.value,
+          subregionId: this.subregionSelect.value ? this.subregionSelect.value.id : null,
+          latitude: this.addMarkerLat,
+          longitude: this.addMarkerLng
+        };
+        this.mapService.postArea(areaModel).subscribe(data => {
+          this.handlePosted();
+        },
+        error => {
+          console.error(error);
+        })
+        break;
+
+      // Subregion
+      case "2":
+        var subregionModel = {
+          name: this.addNameControl.value,
+          regionId: this.regionSelect.value ? this.regionSelect.value.id : null,
+          latitude: this.addMarkerLat,
+          longitude: this.addMarkerLng
+        };
+        this.mapService.postSubregion(subregionModel).subscribe(data => {
+          this.handlePosted();
+        },
+        error => {
+          console.error(error);
+        })
+        break;
+
+      // Region
+      case "3":
+        var regionModel = {
+          name: this.addNameControl.value,
+          subcontinentId: this.subcontinentSelect.value ? this.subcontinentSelect.value.id : null,
+          latitude: this.addMarkerLat,
+          longitude: this.addMarkerLng
+        };
+        this.mapService.postRegion(regionModel).subscribe(data => {
+          this.handlePosted();
+        },
+        error => {
+          console.error(error);
+        })
+        break;
+
+      // Subcontinent
+      case "4":
+        var subcontinentModel = {
+          name: this.addNameControl.value,
+          continentId: this.continentSelect.value ? this.continentSelect.value.id : null,
+          latitude: this.addMarkerLat,
+          longitude: this.addMarkerLng
+        };
+        this.mapService.postSubcontinent(subcontinentModel).subscribe(data => {
+          this.handlePosted();
+        },
+        error => {
+          console.error(error);
+        })
+        break;
+
+      // Continent
+      case "5":
+        console.log(this.celestialObjectSelect);
+        var continentModel = {
+          name: this.addNameControl.value,
+          continentId: this.celestialObjectSelect.value ? this.celestialObjectSelect.value.id : null,
+          latitude: this.addMarkerLat,
+          longitude: this.addMarkerLng
+        };
+        this.mapService.postContinent(continentModel).subscribe(data => {
+          this.handlePosted();
+        },
+        error => {
+          console.error(error);
+        })
+        break;
+    }
+  }
+
+  public handlePosted() {
+    this.clearAllMarkerLayers();
+    this.queryLocations();
+    this.placing = false;
+    this.addLocationMarkerIcon = 'add';
+    this.addMarkerLayer.clearLayers();
+    this.addNameControl.setValue('');
+    this.addNameControl.markAsUntouched();
+    this.queryLocationTypes();
+    this.queryAreas();
+    this.querySubregions();
+    this.queryRegions();
+    this.querySubcontinents();
+    this.queryContinents();
+    this.queryCelestialObjects();
+    this.applyDivIcons(this.areas);
   }
 
   public addMarkers(markers: any): void {
@@ -186,6 +390,22 @@ export class MapComponent implements OnInit {
     }
   }
 
+  public addAreas(): void {
+    this.applyDivIcons(this.areas);
+  }
+
+  public applyDivIcons(icons: any): void {
+    for (var i = 0; i < icons.length; i++) {
+      var icon = icons[i];
+      if (!(icon.latitude && icon.longitude)) continue;
+
+      L.divIcon({ 
+        html: "<div>" + icon.name + "</div>"
+      }).addTo(this.areaLayer);
+      this.areaLayer.addTo(this.map);
+    }
+  }
+
   public showMarkers() {
     var zoom = this.map.getZoom();
     if (zoom < this.primaryMarkerLayerZoom) {
@@ -196,12 +416,26 @@ export class MapComponent implements OnInit {
       this.map.addLayer(this.primaryMarkerLayer);
     }
   }
-
   constructor(private mapService: MapService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.queryLocationTypes();
+    this.queryAreas();
+    this.querySubregions();
+    this.queryRegions();
+    this.querySubcontinents();
+    this.queryContinents();
+    this.queryCelestialObjects();
     this.initMap();
     this.map.on("zoomend", (e: any) => {this.showMarkers();});
+
+    /*// Adds a layer
+    var ciLayer = L.canvasIconLayer({}).addTo(this.map);
+
+    // Marker definition
+    var marker =  L.marker([58.5578, 29.0087]);
+
+    // Adding marker to layer
+    ciLayer.addMarker(marker);*/
   }
 }
